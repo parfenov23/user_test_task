75a19207dda2c466bf010666619a542bcfdb7749
maks <prospu@qip.ru>
2017-09-03 18:15:42 +0500

    fix

--- public/js/project261770/app.js
+++ public/js/project261770/app.js
@@ -1,9 +1,9 @@
 var redirectToCurrentCity = function(){
    var curr_city = $.cookie('city') || ymaps.geolocation.city;
    var find_url = $("#study_popup2 a[data-city='"+ curr_city +"']").attr("href");
-   if ( window.location.href.search(find_url) < 0){
-       window.location.href = find_url;
-   } 
+   // if ( window.location.href.search(find_url) < 0){
+   //     window.location.href = find_url;
+   // } 
 }
 $(document).ready(function(){
     setTimeout(function(){
